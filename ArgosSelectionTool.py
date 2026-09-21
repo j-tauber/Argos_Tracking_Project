0f6b3ebad33e1510ea4e1c2251a2e0d5381dd391
j-tauber <jamestauber@icloud.com>
2026-09-21 12:56:57 -0400

    3.4 up to the beginning of task 5

--- ArgosSelectionTool.py
+++ ArgosSelectionTool.py
@@ -10,9 +10,36 @@
 #--------------------------------------------------------------
 
 # Create the geographic selection box
+
 the_box = {
-    'x_min' : 34.00,
-    'y_min' : -76.00,
-    'x_max' : 34.50,
-    'y_max' : -75.00
+    'x_min': 34.00,
+    'y_min': -76.0,
+    'x_max': 34.50,
+    'y_max': -75.00
 }
+
+
+# Copy and paste a line of data as the lineString variable value
+lineString = '10186609169,true,2019-05-16 23:41:50.000,-76.52193999999997,31.81911,,0.0,-120.0,4.0167967421E8,261.0,222,"56",31.81911,31.81911,"2",-76.52193999999997,-76.52193999999997,9,0,3,98.0,508.0,805.0,84.0,6,5,0,1,"1",,,"argos-doppler-shift","Pterodroma hasitata","174441","HA09","Satellite tracking of black-capped petrels, 2019"'
+    
+# Use the split command to parse the items in lineString into a list object
+line_data = lineString.split(',')
+  
+# Assign variables to specfic items in the list
+event_id = line_data[0]   # Argos tracking event ID ("event-id")
+timestamp = line_data[2]  # Observation date ("timestamp")
+lat = line_data[4]        # Observation latitude  ("location-lat")
+lon = line_data[3]        # Observation longitude ("location-lon")
+lc  = line_data[14]        # Observation location class ("argos:lc")
+tag_id = line_data[-3]     # Tag identifier ("tag-local-identifier")
+
+lat = float(line_data[4])        # Observation latitude  ("location-lat")
+lon = float(line_data[3])        # Observation longitude ("location-lon")
+lat_condition = the_box['y_min'] < lat < the_box['y_max']
+lon_condition = the_box['x_min'] < lon < the_box['x_max']
+
+#Report the status of the points
+if lat_condition & lon_condition:
+    print(f'Record {event_id}: {tag_id} was IN the box at {timestamp}')
+else:
+    print(f'Record {event_id}: {tag_id} was NOT IN the box at {timestamp}')
